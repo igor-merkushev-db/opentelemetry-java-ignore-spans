dab62a06c3be63a6815228d527b1b86db3a650e7
igor-merkushev-db <129967047+igor-merkushev-db@users.noreply.github.com>
2023-06-15 13:51:03 +0000

    rm smoke tests from build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "3.0.5"
+        id("org.springframework.boot") version "2.7.10"
         id("io.spring.dependency-management") version "1.1.0"
         id("de.undercouch.download") version "5.4.0"
         id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -11,4 +11,3 @@ pluginManagement {
 rootProject.name = "opentelemetry-java-ignore-spans"
 
 include("extension")
-include("springboot-actuator-smoke-test")
